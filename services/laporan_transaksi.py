c9894a2b437d78fd129e19aaa90c327a01dece1a
harvestscry <harvestscary@gmail.com>
2025-12-14 23:08:01 +0700

    ...

--- services/laporan_transaksi.py
+++ services/laporan_transaksi.py
@@ -3,11 +3,11 @@ from data.storage import transaksi, daftar_kategori
 def laporan_pengeluaran_boros():
     print("== Daftar Pengeluaran ==")
     
-    batas_boros = 1000000
+    batas_boros = 1000000 
     
     for item in transaksi:
-        total = sum(t["jumlah"] for t in transaksi)
         if item["jumlah"] > batas_boros:
-            print(f"PERINGATAN KATEGORI BOROS:")
-            print(f"{item}: Rp.{total}")
-            
+            kategori = item.get("kategori", "Tidak diketahui")
+            total = item["jumlah"]
+            print("PERINGATAN KATEGORI BOROS:")
+            print(f"Kategori: {kategori} | Jumlah: Rp.{total:,}")
